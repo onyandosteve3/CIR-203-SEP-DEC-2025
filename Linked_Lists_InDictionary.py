11ab39407132c88b0a7243bd5ba7b9f98e26ed8b
stephene <otiwilly85@gmail.com>
2025-12-04 17:57:09 -0300

    Add files via upload

---
+++ Linked_Lists_InDictionary.py
@@ -0,0 +1,14 @@
+Students_info={
+                'Name':"Steve",
+                    next:{
+                        'Adm':"CIM/00053/023",
+                            next:{
+                                'Units': "CIR_203,CIR_205,CIM_207",
+                                    next:{
+                                        'Grades': "91,92,93",
+                                           next: {}
+                                            }
+                                    }
+                         }
+    }
+print(Students_info)
